36bbf2bcf08ea91dbb6d22b03914e960b62ed30c
raja28april <raja28april@gmail.com>
2022-01-20 19:35:51 +0530

    Raja:understanding data types

---
+++ dataTypes.js
@@ -0,0 +1,34 @@
+let int = 10;
+console.log(typeof(int));
+const finalInt =10;
+console.log(typeof(finalInt));
+
+let str = "raja"
+console.log(typeof(str));
+
+let float = 10.09
+console.log(typeof(float));
+
+
+let boolean = true
+console.log(typeof(boolean));
+
+
+let fruits =['apple','mango']
+console.log(typeof(fruits));
+
+let testData = [{
+    "username":"raja",
+    "password":"pass"
+},
+{
+    "username":"raja1",
+    "password":"pass1"
+}
+]
+console.log(typeof(testData));
+
+let nullValue=null;
+console.log(typeof(nullValue));
+let undefinedValue;
+console.log(typeof(undefinedValue));
