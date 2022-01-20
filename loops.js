9a8451cc2dc3f2cbc502b476a75fd36ea3f1fe44
raja28april <raja28april@gmail.com>
2022-01-20 19:36:07 +0530

    Raja:understanding loops

---
+++ loops.js
@@ -0,0 +1,18 @@
+//while loop
+let i =0;
+while(i<3){
+    i++;
+    console.log("while loop - value of i is "+i)
+}
+//do while loop
+let j=0;
+do{
+    j++;
+    console.log("do-while loop - value of j is "+j)
+}while(i<3);
+
+//for loop
+
+for(let i=0; i<3;i++){
+    console.log("For loop - value of index is"+i)
+}
