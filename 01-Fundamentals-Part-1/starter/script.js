434a6a0cecbf1019b0645cb94009926312df8d46
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-09-08 19:05:12 +0200

    Raja:Data types fundamentals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,23 +1,47 @@
-let js ='amazing'
-if(js==='amazing') alert('Javascript is FUN!')
-console.log(21+33)
+// let js ='amazing'
+// if(js==='amazing') alert('Javascript is FUN!')
+// console.log(21+33)
 
 
-let firstName = 'Raja';
-console.log(firstName)
+// let firstName = 'Raja';
+// console.log(firstName)
 
-//variable name convention
-let raja_ramasamy = 'RR';
-let $function = 27;
-let person = 'Raja';
+// //variable name convention
+// let raja_ramasamy = 'RR';
+// let $function = 27;
+// let person = 'Raja';
 
-let pi = 3.14;
-let PI = 3.1415;
+// let pi = 3.14;
+// let PI = 3.1415;
 
-let myFirstJob = 'teacher';
-let myCurrentJob = 'coder';
+// let myFirstJob = 'teacher';
+// let myCurrentJob = 'coder';
 
-let job1 = 'Teacher';
-let job2 = 'Coder';
+// let job1 = 'Teacher';
+// let job2 = 'Coder';
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
+
+//Datatypes
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);//true
+
+console.log(typeof true);//boolean
+console.log(typeof javascriptIsFun);//boolean
+console.log(typeof 23);//number
+console.log(typeof 'Raja');//string
+
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);//string
+
+
+let year;
+console.log(year);//undefined
+console.log(typeof year);//undefined
+
+year = 1991;
+console.log(typeof year);//number
+
+console.log(typeof null);//object
