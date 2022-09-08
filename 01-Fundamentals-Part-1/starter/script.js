e794ea498b6a5ce6b8d6d4d24134b533f80e31b3
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-09-08 18:56:10 +0200

    Raja:naming convention fundamentals

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+let js ='amazing'
+if(js==='amazing') alert('Javascript is FUN!')
+console.log(21+33)
+
+
+let firstName = 'Raja';
+console.log(firstName)
+
+//variable name convention
+let raja_ramasamy = 'RR';
+let $function = 27;
+let person = 'Raja';
+
+let pi = 3.14;
+let PI = 3.1415;
+
+let myFirstJob = 'teacher';
+let myCurrentJob = 'coder';
+
+let job1 = 'Teacher';
+let job2 = 'Coder';
+
+console.log(myFirstJob);
