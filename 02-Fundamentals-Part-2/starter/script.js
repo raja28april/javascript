2c111de490a163114a6326e3821f140ec604652e
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-09-09 11:49:02 +0200

    Raja:use strict mode in js-syntax err while using reserved words

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,11 @@
-// 'use strict';//if not used then there wont be any findings of indefined variables as given in line 6 "hasDrivingsLicense"
+'use strict';//if not used then there wont be any findings of indefined variables as given in line 6 "hasDrivingsLicense"
 
 let hasDrivingLicense = false;
 const passTest=true;
 
-if(passTest) hasDrivngLicense=true;//hasDrivingLicense is misspelled 
+if(passTest) hasDrivingLicense=true;//hasDrivingLicense is misspelled 
 if(hasDrivingLicense) console.log('I can drive😎');
+
+// const interface = 10;//SyntaxError: Unexpected strict mode reserved word when using strict mode
+
+const private = 20;//SyntaxError: Unexpected strict mode reserved word when using strict mode
