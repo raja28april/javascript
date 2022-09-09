8b4eb04cc5c2aab4bff0c8218d1443b42ab58ef3
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-09-09 11:47:02 +0200

    Raja:use strict mode in js

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,7 @@
+// 'use strict';//if not used then there wont be any findings of indefined variables as given in line 6 "hasDrivingsLicense"
+
+let hasDrivingLicense = false;
+const passTest=true;
+
+if(passTest) hasDrivngLicense=true;//hasDrivingLicense is misspelled 
+if(hasDrivingLicense) console.log('I can drive😎');
