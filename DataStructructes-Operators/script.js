070439515eed92761b6beb279098c2fae5f1a7c4
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-09-18 09:59:38 +0200

    Raja: Destructing of arrays

---
+++ DataStructructes-Operators/script.js
@@ -0,0 +1,55 @@
+'use strict';
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    order: function(starter,main){
+        return [this.starterMenu[starter],this.mainMenu[main]];
+    }
+}
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const[x,y,z]= arr;
+console.log(x,y,z);
+
+let [main,secondary] = restaurant.categories;
+console.log(main,secondary);
+
+//swap two values using normal way
+// const temp = main;
+// main = secondary;
+// secondary=temp;
+// console.log(main,secondary);
+
+//swap using destructuring arrays
+[main,secondary] = [secondary,main];
+console.log(main,secondary); 
+
+// console.log(restaurant.order(2,0));
+
+//receive 2 return values from a function
+const[starterCourse,mainCourse]  = restaurant.order(2,0);
+console.log(starterCourse,mainCourse);
+
+//NESTED Destructing
+const nested = [2,3,[4,5]];
+// const[i,j]=nested;
+const[i,,[j,k]]=nested;
+// console.log(i,j);
+console.log(i,j,k);
+
+
+//DEFAULT VALUES while destructuring 
+const values = [1,3,4];
+// const[r,s,t,u] = values;
+// console.log(r,s,t,u);//u value will be undefined
+
+const[r,s=5,t=2,u=1] = values;
+console.log(r,s,t,u);
+
