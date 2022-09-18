cb02b6ae3f14288428e3ce70e65843bbec2c81ae
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-09-18 20:43:35 +0200

    Raja:Enhanced Object literals - ES6

--- DataStructructes-Operators/script.js
+++ DataStructructes-Operators/script.js
@@ -1,4 +1,19 @@
 'use strict';
+const weekdays = ['mon','tue','wed','thu','fri','sat','sun'];
+const openingHours= {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekdays[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
 const restaurant = {
     name: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -6,32 +21,24 @@ const restaurant = {
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            open: 0, // Open 24 hours
-            close: 24,
-        },
-    },
-    order: function (starter, main) {
-        return [this.starterMenu[starter], this.mainMenu[main]];
+    // openingHours: openingHours,//before ES6
+    openingHours,//from ES6 enhanced object literals
+
+    // order: function (starterIndex, mainIndex) {
+    //     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    // },//before ES6
+    order(starterIndex, mainIndex) {//From ES6 - enhanced way of creating methods
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
 
     //Destructuring of objects with defaults too
-    orderDelivery: function ({ starterIndex = 0, mainIndex = 1, time = '23:20', address }) {
+    orderDelivery({ starterIndex = 0, mainIndex = 1, time = '23:20', address }) {
         console.log(`Order received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
     },
-    orderPasta: function (ing1, ing2, ing3) {
+    orderPasta(ing1, ing2, ing3) {
         console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
     },
-    orderPizza: function (mainIngredient, ...otherIngredients) {
+    orderPizza(mainIngredient, ...otherIngredients) {
         console.log(mainIngredient);
         console.log(otherIngredients);
     }
@@ -45,7 +52,7 @@ for(const item of menu) console.log(item);
 for(const item of menu.entries()) console.log(item);
 // console.log(...menu.entries());
 for(const item of menu.entries()) console.log(`${item[0]+1}: ${item[1]}`);
-for(const [i,el] of menu.entries()) console.log(`${i}: ${el}`);//with destructing of arrays 
+for(const [i,el] of menu.entries()) console.log(`${i}: ${el}`);
 
 
 
