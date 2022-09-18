880656f6eb28a9908a827a5c17702a70dc099639
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-09-18 21:13:51 +0200

    Raja: Looping Objects: Object Keys, Values, and Entries

--- DataStructructes-Operators/script.js
+++ DataStructructes-Operators/script.js
@@ -44,7 +44,31 @@ const restaurant = {
     }
 }
 
+///////////////Looping Objects: Object Keys, Values, and Entries///////
+
+//Property Names
+const properties = Object.keys(openingHours);
+console.log(properties);
+let openStr = `We are open on ${properties.length} days`;
+for(const day of properties){
+    openStr+=`${day},`;
+}
+console.log(openStr);
+
+//Property Values
+const values = Object.values(openingHours);
+console.log(values);
+
+//Entire Object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for(const [day,{open,close}] of entries){
+    console.log(`On ${day}, we are open at ${open} and close at ${close}`);
+}
+
 ///////////////OPTIONAL CHAINING ?. OPERTAOR////////////////
+/*
 if (restaurant.openingHours && restaurant.openingHours.mon) {
     console.log(restaurant.openingHours.mon.open);
 }
@@ -61,6 +85,8 @@ console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exists');
 //Optional chaining on Arrays
 const users = [{ name: 'Raja', email: 'hello@raja.in' }];
 console.log(users[0]?.name ?? 'Array is empty');
+*/
+
 
 /////////////Looping arrays - 'For of' loop
 /*
