411ec66fe1490a29cdc3272a9bc31fc217afbcce
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-09-20 16:40:22 +0200

    Raja: Solved Coding Challenge 3

--- DataStructructes-Operators/CodingChallenge3.js
+++ DataStructructes-Operators/CodingChallenge3.js
@@ -28,3 +28,20 @@ const gameEvents = new Map([
   ]);
   
   // 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2. 
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3. 
+console.log(`An event happened, on average, every ${90/gameEvents.size} minutes`);
+const time = [...gameEvents.keys()].pop();
+console.log(`An event happened, on average, every ${time/gameEvents.size} minutes`);
+
+// 4.
+for(const [key,value] of gameEvents){
+    const half = key<90/2?'FIRST':'SECOND';
+        console.log(`[${half} HALF] ${key}: ${value}`);
+}
