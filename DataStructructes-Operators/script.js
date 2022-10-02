890fab2c5d1a7049e8a9c0e81acaad0fe1d6f998
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-02 09:06:59 +0200

    Raja: Strings part - 2

--- DataStructructes-Operators/script.js
+++ DataStructructes-Operators/script.js
@@ -44,7 +44,63 @@ const restaurant = {
     }
 }
 
-//////////////////////Strings/////////////////
+//////Strings - Part 2/////////////////
+
+const airline = 'TAP Air Portugal';
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+//Fix Capitalization in name
+const passenger = 'RaJa';//Raja
+const passengerLowerCase = passenger.toLowerCase();
+const passengerCorrect = passengerLowerCase[0].toUpperCase()+passengerLowerCase.slice(1);
+console.log(passengerCorrect);
+
+//Comparing emails
+const email = 'hello@raja.io';
+const loginEmail = '   Hello@raja.Io ';
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email===normalizedEmail);
+
+//replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£','$').replace(',','.');
+
+console.log(priceUS);
+
+const announcement = 'All passengers come to boarding door 23. Boarding door 23!';
+console.log(announcement.replace('door','gate'));
+console.log(announcement.replaceAll('door','gate'));
+console.log(announcement.replace(/door/g,'gate'));
+
+//Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+
+
+if(plane.startsWith('Airbus') && plane.endsWith('neo')){
+    console.log('Part of the new Airbus family');
+}
+
+//Practice exercise
+const checkBaggage = function(items){
+    const baggage = items.toLowerCase();
+    if (baggage.includes('knife') || baggage.includes('gun')) {
+        console.log('You are NOT allowed on board');
+      } else {
+        console.log('Welcome aboard!');
+      }
+}
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+
+//////////////////////Strings-Part 1/////////////////
+/*
 const airline = 'TAP Air Portugal';
 const plane = 'A320';
 
@@ -85,15 +141,6 @@ checkMiddleSeat('5E');
 console.log(new String('jonas'));
 console.log(typeof new String('jonas'));
 console.log(typeof new String('jonas').slice(1));
-
-
-
-
-
-
-
-
-
 /*
 ////////////////Maps//////////////////////
 
