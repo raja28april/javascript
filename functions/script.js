53c69f62bcda5d093d84c21bf7f6d7061a76838d
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-07 09:18:54 +0200

    Raja: Call and apply methods

--- functions/script.js
+++ functions/script.js
@@ -1,4 +1,54 @@
 'use strict';
+/////////////Call and apply methods/////////////
+const lufthansa = {
+    airline: 'Lufthansa',
+    iatacode: 'LH',
+    bookings: [],
+    // book: function(){}
+    book(flightNum, name) {
+        console.log(
+            `${name} booked a seat on ${this.airline} flight ${this.iatacode}${flightNum}`
+        );
+        this.bookings.push({flight: `${this.iatacode}${flightNum}`,name});
+    },
+}
+lufthansa.book(239, 'Raja Ramasamy');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+const eurowings = {
+    airline: 'Eurowings',
+    iatacode: 'EW',
+    bookings: [],
+};
+
+const book = lufthansa.book;
+//Does NOT work
+// book(23,'Raja Ramasa')
+
+/**
+ * "Call Method"
+ * this keyword in lufthansa object when called in previous line points to const book initialized in above line
+ * and so when used call method on a function 'book' we can pass a parameter 
+ * which can be considered as the object to be used as the current object.
+ */
+book.call(eurowings,23,'Raja Ramasa');
+book.call(lufthansa,45,'Ramasam Raja');
+
+const swiss = {
+    airline:'Swiss airlines',
+    iatacode: 'LX',
+    bookings:[]
+};
+
+book.call(swiss,21,'Mary Cooper');
+
+//Apply method
+const flightData = [583,'Mary Cooper'];
+book.apply(swiss,flightData);
+console.log(swiss);
+
+book.call(swiss,...flightData)
+
 ///////////////////////Default Parameters/////////////
 /*
 const bookings = [];
@@ -55,6 +105,7 @@ document.body.addEventListener('click',high5);
 ['Raja','Martha','Partha'].forEach((high5));
 */
 ////////////Functions returning functions////////////
+/*
 const greet = function (greeting) {
     return function (name) {
         console.log(`${greeting} ${name}`);
@@ -70,3 +121,5 @@ greet('Hello')('Raja');
 
 const greetArrow = (greeting) => (name) => console.log(`${greeting} ${name}`);
 greetArrow('Hi')('Raja');
+*/
+
