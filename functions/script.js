1b7500f97ebfccbe55e6c05ed6c36acdf4041e11
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-07 07:14:17 +0200

    Raja: Functions Accepting Callback Functions

--- functions/script.js
+++ functions/script.js
@@ -24,4 +24,35 @@ let a;
 if(a) console.log('true')
 else console.log('false');
 
+///////////////FunctionsAcceptingCallbackFunctions////////////
+
+const oneword = function (str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function (str) {
+    const [firstWord, ...others] = str.split(' ');
+    return [firstWord.toUpperCase(), ...others].join(' ');
+}
+
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+    console.log(`Tansformed by: ${fn.name}`);
+}
+
+transformer('Javascript is the best', oneword);
+transformer('Javascript is the best', upperFirstWord);
+//JS uses callback all the time
+const high5 = function(){
+    console.log('👋');
+}
+
+document.body.addEventListener('click',high5);
+
+
+['Raja','Martha','Partha'].forEach((high5));
+
+
+
 
