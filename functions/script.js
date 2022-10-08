1ed5bac855b397b58faf2018a33cbb1a264cd773
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-08 10:34:14 +0200

    Raja: Coding challenge 2 - closures

--- functions/script.js
+++ functions/script.js
@@ -1,7 +1,20 @@
 'use strict';
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
 
 /////////////////Closures////////////////
-
+/*
 const secureBooking = function () {
     let passengerCount = 0;
     return function () {
@@ -51,8 +64,7 @@ const boardingPassengers = function(n,wait){
 //scope is given to closures only and hence the below perGroup value is NOT taken
 const perGroup = 100;
 boardingPassengers(180,3);
-
-
+*/
 
 
 /////////////////Immediately Invoked Function Expression////////////
