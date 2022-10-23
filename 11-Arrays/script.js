eadce924dcbed5076afb29d5d1b3d6dda8bdf2de
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-23 11:00:38 +0200

    Raja: Implementing Login

--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -87,20 +87,20 @@ const calcDisplayBalance = function (movements) {
 
 calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const out = movements
+  const out = acc.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter((mov) => mov > 0)
-    .map((mov) => (mov * 1.2) / 100)
+    .map((mov) => (mov * acc.interestRate) / 100)
     .filter((int, i, arr) => {
       console.log(arr);
       return int >= 1;
@@ -109,7 +109,7 @@ const calcDisplaySummary = function (movements) {
 
   labelSumInterest.textContent = `${interest}€`;
 };
-calcDisplaySummary(account1.movements);
+calcDisplaySummary(account1);
 
 const user = "Steven Thomas Williams";
 const createUserNames = function (accs) {
@@ -122,6 +122,33 @@ const createUserNames = function (accs) {
   });
 };
 createUserNames(accounts);
+
+//Event Handler
+let currentAccount;
+btnLogin.addEventListener('click',function(e){
+  //Prevent form from submitting
+  e.preventDefault();
+  currentAccount = accounts.find(acc=>acc.userName===inputLoginUsername.value);
+  console.log(currentAccount);
+  if(currentAccount?.pin===Number(inputLoginPin.value)){
+    //Display UI and message
+    labelWelcome.textContent = `Welcome back ${currentAccount.owner.split(' ')[0]}`;
+    containerApp.style.opacity = 100;
+
+    // clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+
+    // Display movements 
+    displayMovements(currentAccount.movements);
+
+    // Display balance
+    calcDisplayBalance(currentAccount.movements);
+
+    // Display summary
+    calcDisplaySummary(currentAccount);
+
+  }
+});
 
 ///////////////////////////////////////
 // Coding Challenge #1
@@ -399,6 +426,7 @@ calcAverageHumanAge(dogsAges);
 */
 
 //find Method
+/*
 const firstWithdrawl = movements.find(mov=>mov<0);
 console.log(firstWithdrawl);
 
@@ -406,3 +434,4 @@ console.log(accounts);
 
 const account = accounts.find(acc=>acc.owner==='Raja Ramasamy');
 console.log(account);
+*/
