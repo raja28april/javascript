6aad349beb5bba62bd959286992c8eef994a3d0b
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-23 08:09:20 +0200

    Raja: coding challenge -2

--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -279,7 +279,7 @@ console.log(withdrawal);
 */
 //////////////Array Reduce method - data transaformation////////////
 
-console.log(movements);
+/*console.log(movements);
 // the first parameter is the accumulator - like snow ball
 const balance = movements.reduce((acc, curr, i, arr)=> acc + curr,0);
 console.log(balance);
@@ -297,4 +297,43 @@ const max = movements.reduce((acc,mov)=> {
   }
 },movements[0]);
 console.log(max);
+*/
 
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = function(ages){
+
+const humanAges = ages.map((age)=>{
+  return age<=2?2*age:16+age*4;
+});
+const adults = humanAges.filter(age=>age>=18);
+console.log(adults);
+
+// const averageHumanAge = adults.reduce((acc,age)=>{
+//   return acc+age
+// },0)/adults.length;
+
+const averageHumanAge = adults.reduce((acc,age,i,arr)=>{
+  return acc+age/arr.length
+},0);
+console.log(averageHumanAge); 
+}
+const dogsAges = [5, 2, 4, 1, 15, 8, 3];
+calcAverageHumanAge(dogsAges);
