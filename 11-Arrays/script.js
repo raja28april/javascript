d16c7a4843840259a826e444ec33a82be1309e3a
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-24 16:49:07 +0200

    Raja:findIndex method usage

--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -182,6 +182,24 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.userName &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.userName === currentAccount.userName
+    );
+    //Delete account
+    accounts.splice(index, 1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
 ///////////////Lectures////////////////////////
 // Coding Challenge #1
 
