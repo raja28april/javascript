e55d5a836884dfe66bae9c66c410eefce28bae14
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-25 18:29:08 +0200

    Raja: creating, inserting and deleting elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -31,3 +31,48 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+/////Selecting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSelections = document.querySelectorAll('.section');
+console.log(allSelections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+//////////////////////////////////////////
+// Creating and inserting elements
+//.insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved function and anlaytics. <button class = "btn btn--close-cookie">Got it</button>';
+message.innerHTML = 'We use cookies for improved function and anlaytics. <button class = "btn btn--close-cookie">Got it!</button>';
+
+header.prepend(message);//prepend method adds element as first child
+header.append(message);//append method adds element as its last child
+
+// header.append(message.cloneNode(true));
+
+header.before(message);//adds message element before the header element
+header.after(message);//adds message element after the header element
+
+//Delete Elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    //message.remove();
+    message.parentElement.removeChild(message);
+  })
+///////////////////////////////////////////
+
+
