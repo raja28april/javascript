a530b1f4a65e9734fe2d8bb431dad6507879123d
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-25 20:51:02 +0200

    Raja: types of events and event listeners

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -150,7 +150,29 @@ btnScrollTo.addEventListener('click', function (e) {
   // });
 
   section1.scrollIntoView({ behavior: 'smooth' });
-})
+});
+///////////////////////////////////////////
+//Type of events and events handler
+const h1 = document.querySelector('h1');
+
+// h1.addEventListener('mouseenter', function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// });
+
+//this is another way
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter:  Great! You are reading the heading :D');
+// }
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D')
+
+  // h1.removeEventListener('mouseenter', alertH1);
+
+}
+h1.addEventListener('mouseenter', alertH1);
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
 
 
 
