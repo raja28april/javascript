16412129362e5f3479b4229e9c50c8f7d9164a6f
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-27 07:22:49 +0200

    Raja: Prototypes in classes

--- 14-oop/starter/script.js
+++ 14-oop/starter/script.js
@@ -31,4 +31,31 @@ Person.hey = function () {
 };
 
 Person.hey();
-console.log("Per:" + Person);  
+console.log("Per:" + Person);
+
+///////////////////////////////////////
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log("__proto__: " + jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototyeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
