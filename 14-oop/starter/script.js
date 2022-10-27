4263f3ff1b35a78e0908578a9ff5ebf3625b352e
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-27 09:28:41 +0200

    Raja: Object.create function

--- 14-oop/starter/script.js
+++ 14-oop/starter/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 ///////////////////////////////////////
 // Constructor Functions and the new Operator
 const Person = function (firstName, birthYear) {
@@ -81,10 +82,11 @@ Array.prototype.unique = function () {
 
 console.log(arr.unique());
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 console.dir(x => x + 1);
 
 
+*/
 ///////////////////////////////////////
 // Coding Challenge #1
 
@@ -99,7 +101,7 @@ DATA CAR 2: 'Mercedes' going at 95 km/h
 
 GOOD LUCK 😀
 */
-
+/*
 const Car = function (make, speed) {
     this.make = make;
     this.speed = speed;
@@ -153,13 +155,14 @@ class PersonCl {
 
     // Set a property that already exists
     set fullName(name) {
+        console.log(name);
         if (name.includes(' ')) this._fullName = name;
         else alert(`${name} is not a full name!`);
     }
 
-    get fullName() {
-        return this._fullName;
-    }
+    // get fullName() {
+    //     return this._fullName;
+    // }
 
     // Static method
     static hey() {
@@ -185,5 +188,51 @@ jessica.greet();
 // 3. Classes are executed in strict mode
 
 const walter = new PersonCl('Walter White', 1965);
-  // PersonCl.hey();
+// PersonCl.hey();
 
+///////////////////////////////////////
+// Setters and Getters
+const account = {
+    owner: 'Raja',
+    movements: [200, 530, 120, 300],
+
+    get latest() {
+        return this.movements.slice(-1).pop();
+    },
+
+    set latest(mov) {
+        this.movements.push(mov);
+    }
+}
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
+
+*/
+
+///////////////////////////////////////
+// Object.create
+const PersonProto = {
+    calcAge() {
+        console.log(2037 - this.birthYear);
+    },
+
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
