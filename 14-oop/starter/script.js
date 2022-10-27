be8c6acbd8fa8f57e58c8cc85993f7164354700a
raja.ramasamy <raja.ramasamy@elinvar.de>
2022-10-27 09:29:29 +0200

    Raja: Coding Challenge-1

--- 14-oop/starter/script.js
+++ 14-oop/starter/script.js
@@ -90,7 +90,7 @@ console.dir(x => x + 1);
 ///////////////////////////////////////
 // Coding Challenge #1
 
-/* 
+/*
 1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
@@ -214,6 +214,7 @@ console.log(account.movements);
 
 ///////////////////////////////////////
 // Object.create
+/*
 const PersonProto = {
     calcAge() {
         console.log(2037 - this.birthYear);
@@ -236,3 +237,16 @@ console.log(steven.__proto__ === PersonProto);
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1979);
 sarah.calcAge();
+*/
+/////////////////////////////////////// Coding Challenge #2
+
+/*
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+ 
+DATA CAR 1: 'Ford' going at 120 km/h
+ 
+GOOD LUCK 😀
+*/
